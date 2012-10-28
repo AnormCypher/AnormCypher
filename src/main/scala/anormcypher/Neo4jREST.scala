cfe4979c6783ae118721ccd516d2696b49c1d033
Wes Freeman <freeman.wes@gmail.com>
2012-10-28 09:51:51 -0400

    adding new case classes and returning a sequence of maps for cypher results.

--- src/main/scala/anormcypher/Neo4jREST.scala
+++ src/main/scala/anormcypher/Neo4jREST.scala
@@ -2,21 +2,51 @@ package anormcypher
 
 import dispatch._
 import com.codahale.jerkson.Json._
+import java.util.LinkedHashMap
+import scala.collection.JavaConverters._
 
 object NeoRESTConnection {
   // TODO: read from properties
   val baseURL = "http://localhost:7474/db/data/"
 
-  def sendQuery(stmt: CypherStatement): CypherRESTResult = {
+  def sendQuery(stmt: CypherStatement): Seq[Map[String, Any]] = {
     val cypherRequest = url(baseURL + "cypher").POST <:< Map("accept" -> "application/json", "content-type" -> "application/json")
     cypherRequest.setBody(generate(stmt))
     val strResult = Http(cypherRequest OK as.String)
-    val restResult = strResult()
-    println("RESTResult: " + restResult)
-    parse[CypherRESTResult](restResult)
+    // TODO: make not blow for exception cases
+    val cypherRESTResult = parse[CypherRESTResult](strResult())
+    // build a sequence of Maps for the results
+    cypherRESTResult.data.map { 
+      d => d.zipWithIndex.map { 
+        case (e, idx) => (cypherRESTResult.columns(idx), e)
+      }.toMap 
+    }
+  }
+
+  // TODO: convert this stuff to use json4s, or something that
+  // doesn't force the use of Java->Scala conversions.
+  // didn't realize Jerkson did that.
+  def asNeoNode(n:Any):NeoNode = {
+    val node = n.asInstanceOf[LinkedHashMap[String, Any]].asScala
+    val self = node("self").asInstanceOf[String]
+    val id = self.substring(self.lastIndexOf("/") + 1).toLong
+    NeoNode(id, node("data").asInstanceOf[LinkedHashMap[String, Any]].asScala.toMap)
+  }
+
+  def asNeoRelationship(r:Any):NeoRelationship = {
+    val rel = r.asInstanceOf[LinkedHashMap[String, Any]].asScala
+    val self = rel("self").asInstanceOf[String]
+    val id = self.substring(self.lastIndexOf("/") + 1).toLong
+    val endStr = rel("end").asInstanceOf[String]
+    val end = endStr.substring(endStr.lastIndexOf("/") + 1).toLong
+    val startStr = rel("start").asInstanceOf[String]
+    val start = startStr.substring(startStr.lastIndexOf("/") + 1).toLong
+    NeoRelationship(id, rel("data").asInstanceOf[LinkedHashMap[String,Any]].asScala.toMap, start, end)
   }
 }
 
 case class CypherStatement(query:String, params:Map[String, Any] = Map())
-
 case class CypherRESTResult(columns: Vector[String], data: Seq[Seq[Any]])
+
+case class NeoNode(id:Long, props: Map[String, Any]) 
+case class NeoRelationship(id:Long, props: Map[String, Any], start:Long, end:Long) 
--- src/test/scala/anormcypher/Neo4jRESTTest.scala
+++ src/test/scala/anormcypher/Neo4jRESTTest.scala
@@ -3,9 +3,11 @@ package anormcyphertest
 import org.scalatest._
 import org.scalatest.matchers._
 import anormcypher._
+import java.util.LinkedHashMap
+import scala.collection.JavaConverters._
 
 class Neo4jRESTSpec extends FlatSpec with ShouldMatchers {
-  val cypherStatement = CypherStatement(query="START n=node(0) RETURN n")
+  val cypherStatement = CypherStatement(query="START n=node(*) MATCH n-[r?]->() RETURN n,1,r")
   //TODO a good way to mock the REST interface would be cool; ideas?
   // for now, this test requires neo4j to be running
   "A NeoRESTConnection" should "be able to connect and make a query without parameters" in {
@@ -13,5 +15,7 @@ class Neo4jRESTSpec extends FlatSpec with ShouldMatchers {
   }
 
   it should "placeholder" in {
+    val results = NeoRESTConnection.sendQuery(cypherStatement)
+    println(results(1)("n").asInstanceOf[LinkedHashMap[String,Any]].asScala("data"))
   }
 }
