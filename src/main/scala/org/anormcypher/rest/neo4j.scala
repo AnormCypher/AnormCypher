fe61d510493861efd29c0fe2c8ac2b9b6a2d4530
Wes Freeman <wes@skeweredrook.com>
2013-12-29 00:56:27 -0500

    making it compile

--- src/main/scala/org/anormcypher/rest/neo4j.scala
+++ src/main/scala/org/anormcypher/rest/neo4j.scala
@@ -1,21 +1,20 @@
 package org.anormcypher.rest
 
-import scala.concurrent.{Future as SFuture}
+import scala.concurrent.{Future => SFuture}
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Success, Failure}
 import org.anormcypher._
 
-case class Neo4jREST(baseURL:String, transURL:String = "", cypherURL:String = "") extends Neo4jConnection {
+case class Neo4jREST(baseURL:String) extends Neo4jConnection {
   def connect = {
-    val base = Http(url(baseURL) OK as.Json)()
-    println(base)
-    Neo4jREST(baseURL=baseURL,transURL=transURL,cypherURL=cypherURL)
+    //Neo4jREST(baseURL=baseURL,transURL=transURL,cypherURL=cypherURL)
   }
 
-  def query(stmt:CypherStatement):Future[Stream[CypherRow]] = future {
+  def query(stmt:CypherStatement):SFuture[Stream[CypherRow]] = concurrent.future {
     Stream[CypherRow]()
   }
 
   //TODO make this timeout configurable
-  def querySync(stmt:CypherStatement):Stream[CypherRow] = Await.result[Stream[CypherRow]](query(stmt), 10.second)
+  def querySync(stmt:CypherStatement):Stream[CypherRow] = ??? //Await.result[Stream[CypherRow]](query(stmt), 10.second)
 }
