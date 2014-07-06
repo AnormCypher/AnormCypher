0d2388e8686a32583ec7bf164117fcfd6e1ad362
Wes Freeman <wes@skeweredrook.com>
2014-07-06 12:37:10 -0400

    Update AnormCypherTest.scala

--- src/test/scala/org/anormcypher/AnormCypherTest.scala
+++ src/test/scala/org/anormcypher/AnormCypherTest.scala
@@ -34,8 +34,8 @@ class AnormCypherSpec extends FlatSpec with Matchers with BeforeAndAfterEach {
   override def afterEach() = {
     // delete the test data
     Cypher("""match (n)
-      optional match n-[r]-()
       where n.tag = "anormcyphertest"
+      optional match n-[r]-()
       delete n, r;
       """)()
   }
